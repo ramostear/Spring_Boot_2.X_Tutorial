ca1a72038286a3c2a28558ad4ccc4cbfa3de865f
ramostear <ramostear@163.com>
2019-04-21 16:48:05 +0800

    第一章的内容源码

---
+++ spring-boot-quick-start/src/test/java/com/ramostear/spring/boot/springbootquickstart/SpringBootQuickStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ramostear.spring.boot.springbootquickstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootQuickStartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
