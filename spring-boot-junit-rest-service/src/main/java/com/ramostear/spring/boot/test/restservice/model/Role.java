f09a24c70e439d1136b25c6997255082fda4c218
ramostear <ramostear@163.com>
2019-05-10 04:16:42 +0800

    使用JUnit对Spring Boot中的Controller进行单元测试

---
+++ spring-boot-junit-rest-service/src/main/java/com/ramostear/spring/boot/test/restservice/model/Role.java
@@ -0,0 +1,13 @@
+package com.ramostear.spring.boot.test.restservice.model;
+
+import lombok.Data;
+
+@Data
+public class Role {
+
+    private String id;
+
+    private String name;
+
+    private String description;
+}
